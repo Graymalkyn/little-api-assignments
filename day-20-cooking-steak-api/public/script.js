1db589a2b28fb260b08d8a5c7c4541133367d012
Mark Shuler <biggraydog@live.com>
2016-08-05 20:30:29 -0500

    initial upload

---
+++ day-20-cooking-steak-api/public/script.js
@@ -0,0 +1,21 @@
+var $table = $('#steak-table');
+
+var promise = $.ajax({
+  url: 'http://tiy.ericsowell.com/api/cookingsteaks'
+});
+
+promise.done(function(data){
+    console.log(data)
+
+for (var i=0; i<data.levels.length; i++) {
+
+  var html =
+    '<tr>' +
+      '<td>' + data.levels[i].name + '</td>' +
+      '<td>' + data.levels[i].description + '</td>' +
+      '<td>' + '<img src="' + data.levels[i].imageUrl + '"/>' + '</td>' +
+    '</tr>';
+
+    $table.append(html);
+  }
+});
