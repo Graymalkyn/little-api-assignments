eb131bba352740c416d30dcd8d774325e9cf9357
Mark Shuler <biggraydog@live.com>
2016-08-07 06:09:51 -0500

    adding practice.js file

---
+++ day-20-cooking-steak-api/public/practice.js
@@ -0,0 +1,21 @@
+var $table = $('#steak-table');
+
+var promise = $.ajax({
+  url: 'http://tiy.ericsowell.com/api/cookingsteaks'
+
+});
+
+promise.done(function(data){
+
+  for (var i=0; i<data.levels; i++);
+
+    var html =
+      '<tr>' +
+        '<td>' + data.levels[i].name + '</td>' +
+        '<td>' + data.levels[i].description + '</td>' +
+        '<td>' + '<img src=data.levels[i].imageUrl/>' + '</td>' +
+        <
+      '</tr>'
+         
+
+});
