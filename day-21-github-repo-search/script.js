718d96f31ac7de01236b3416f16a4512f3617d6c
Mark Shuler <biggraydog@live.com>
2016-08-09 08:29:15 -0500

    completed assignment

--- day-21-github-repo-search/script.js
+++ day-21-github-repo-search/script.js
@@ -5,33 +5,26 @@ if (this.RepoSearch === undefined) this.RepoSearch = {};
   var $theList = $('#full-name');
   var $theInput = $('#text-entry');
 
-
-
-  function keyUpHappened(evt){
-    if(evt.keyCode === 13) {
-      start();
+    function keyUpHappened(evt){
+      if(evt.keyCode === 13) {
+        start();
+      }
     }
-  }
 
-  function start() {
-
-
-    var promise = $.ajax({
-      url: 'https://api.github.com/search/repositories?q=' + $theInput.val()
-
-    });
+    function start() {
+      
+      var promise = $.ajax({
+        url: 'https://api.github.com/search/repositories?q=' + $theInput.val()
+      });
 
 
-    promise.done(function(context){
-      $theList.empty();
+      promise.done(function(context){
+        $theList.empty();
+        for (var i=0; i<context.items.length; i++)
+          $theList.append('<li>' + context.items[i].full_name)
+      })
 
-      for (var i=0; i<context.items.length; i++)
-        $theList.append('<li>' + context.items[i].full_name)
-    })
-
-
-
-  }
+    }
 
   context.start = start;
 
